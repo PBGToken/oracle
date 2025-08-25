86195ed926090bf9a13046fedb88d39df5499267
christianschmitz <cschmitz398@gmail.com>
2025-08-25 11:44:34 -0600

    prefetch RWA metadata and coingecko all at once, right after validating (some) prices using minswap pools

--- pwa/src/validators/aws.ts
+++ pwa/src/validators/aws.ts
@@ -17,6 +17,7 @@ import {
     makeShelleyAddress,
     makeValidatorHash,
     MintingPolicyHash,
+    TxOutput,
     type Signature,
     type Tx
 } from "@helios-lang/ledger"
@@ -27,7 +28,7 @@ import {
     makeBlockfrostV0Client
 } from "@helios-lang/tx-utils"
 import { expectDefined } from "@helios-lang/type-utils"
-import { findPool, getAllV2Pools, Pool } from "@helios-lang/minswap"
+import { findPool, getAllV2Pools } from "@helios-lang/minswap"
 import {
     expectConstrData,
     expectIntData,
@@ -170,12 +171,17 @@ const COINGECKO_ASSETS: Record<string, { coingeckoId: string }> = {
     }
 }
 
+type PriceToValidate = {
+    name: string // i.e. the ticker
+    assetClass: AssetClass // the on-chain asset class
+    price: number // on-chain price that must be validated
+    decimals: number
+}
+
 async function validatePrices(
     tx: Tx,
     cardanoClient: BlockfrostV0Client
 ): Promise<void> {
-    const prices: Record<string, number> = {}
-
     const mintedAssetClasses = tx.body.minted.assetClasses.filter(
         (ac) => !ac.isEqual(ADA)
     )
@@ -194,20 +200,53 @@ async function validatePrices(
         output.address.isEqual(addr)
     )
 
-    // TODO: other clients
-    // can't use demeter utxo rpc because it doesn't set the Access-Control-Allow-Origin to *, and setting up a CORS proxy would allow spoofing the returned data with other price data
+    const [pricesToValidate, validationErrors, prices] =
+        await collectPricesToValidate(cardanoClient, assetGroupOutputs)
 
-    let _pools: Pool[] | undefined = undefined
+    await tryValidatingWithMinswapPools(
+        cardanoClient,
+        pricesToValidate,
+        validationErrors
+    )
+    const [coinGeckoPrices, rwas] = await prefetchCoinGeckoPricesAndRWAMetadata(
+        cardanoClient,
+        pricesToValidate
+    )
 
-    const getPools = async (): Promise<Pool[]> => {
-        if (!_pools) {
-            _pools = await getAllV2Pools(cardanoClient)
-        }
+    // this is sync, no more fetching from network needed
+    validateCoinGeckoPrices(coinGeckoPrices, pricesToValidate, validationErrors)
 
-        return _pools
+    // this is async because reserves must be fetched from other networks
+    await validateRWAPrices(
+        coinGeckoPrices,
+        rwas,
+        pricesToValidate,
+        validationErrors
+    )
+
+    for (let name in pricesToValidate) {
+        validationErrors.push(new Error(`Unable to validate price of ${name}`))
     }
 
+    if (validationErrors.length == 1) {
+        throw validationErrors[0]
+    } else if (validationErrors.length > 0) {
+        throw new Error(validationErrors.map((e) => e.message).join("; "))
+    }
+
+    console.log(
+        "Validated tx with prices ",
+        JSON.stringify(prices, undefined, 4)
+    )
+}
+
+async function collectPricesToValidate(
+    cardanoClient: BlockfrostV0Client,
+    assetGroupOutputs: TxOutput[]
+): Promise<[Record<string, PriceToValidate>, Error[], Record<string, number>]> {
+    const pricesToValidate: Record<string, PriceToValidate> = {}
     const validationErrors: Error[] = []
+    const prices: Record<string, number> = {}
 
     for (let output of assetGroupOutputs) {
         if (!output.datum) {
@@ -253,80 +292,199 @@ async function validatePrices(
                 continue
             }
 
-            const pools = await getPools()
-
-            // assume first that the asset is traded on minswap, and look for a minswap pool
-            try {
-                const pool = findPool(pools, makeAssetClass("."), assetClass)
-
-                const adaPerAsset = pool.getPrice(6, decimals)
-
-                if (
-                    Math.abs((price - adaPerAsset) / adaPerAsset) > MAX_REL_DIFF
-                ) {
-                    validationErrors.push(
-                        new Error(
-                            `${name} price out of range, expected ~${adaPerAsset.toFixed(6)}, got ${price.toFixed(6)}`
-                        )
-                    )
-                    continue
-                }
-            } catch (e) {
-                // if minswap pool is found, either something is wrong with minswap (and we fall back to coingecko), or it is an in-house RWA (which will never be traded publicly)
-                if (
-                    e instanceof Error &&
-                    e.message.toLowerCase().includes("no pools")
-                ) {
-                    if (name in COINGECKO_ASSETS) {
-                        console.log(
-                            `No pools found for ${name}, falling back to validating via coingecko price`
-                        )
-
-                        await validateCoingeckoPrice(
-                            name,
-                            price,
-                            validationErrors
-                        )
-                    } else {
-                        console.log(
-                            `No pools found for ${name}, assuming it is an in-house RWA`
-                        )
-
-                        await validateRWAPrices(
-                            cardanoClient,
-                            assetClass,
-                            price,
-                            validationErrors
-                        )
-                    }
-                } else {
-                    throw e
-                }
+            pricesToValidate[name] = {
+                name,
+                assetClass,
+                price,
+                decimals
             }
         }
     }
 
-    if (validationErrors.length == 1) {
-        throw validationErrors[0]
-    } else if (validationErrors.length > 0) {
-        throw new Error(validationErrors.map((e) => e.message).join("; "))
+    return [pricesToValidate, validationErrors, prices]
+}
+
+/**
+ * Removes entries from `pricesToValidate`, and adds errors to `validationErrors`
+ * @param cardanoClient
+ * @param pricesToValidate
+ * @param validationErrors
+ */
+async function tryValidatingWithMinswapPools(
+    cardanoClient: BlockfrostV0Client,
+    pricesToValidate: Record<string, PriceToValidate>,
+    validationErrors: Error[]
+): Promise<void> {
+    if (Object.keys(pricesToValidate).length == 0) {
+        return
     }
 
-    console.log(
-        "Validated tx with prices ",
-        JSON.stringify(prices, undefined, 4)
+    const pools = await getAllV2Pools(cardanoClient)
+
+    for (let name in pricesToValidate) {
+        const { assetClass, price, decimals } = pricesToValidate[name]
+
+        // assume first that the asset is traded on minswap, and look for a minswap pool
+        try {
+            const pool = findPool(pools, makeAssetClass("."), assetClass)
+
+            const adaPerAsset = pool.getPrice(6, decimals)
+
+            if (Math.abs((price - adaPerAsset) / adaPerAsset) > MAX_REL_DIFF) {
+                validationErrors.push(
+                    new Error(
+                        `${name} price out of range, expected ~${adaPerAsset.toFixed(6)}, got ${price.toFixed(6)}`
+                    )
+                )
+            }
+
+            delete pricesToValidate[name]
+        } catch (e) {
+            // if minswap pool is found, either something is wrong with minswap (and we fall back to coingecko), or it is an in-house RWA (which will never be traded publicly)
+            if (
+                e instanceof Error &&
+                e.message.toLowerCase().includes("no pools")
+            ) {
+                console.log(
+                    `No minswap pools found for ${name}, verifying using other methods...`
+                )
+            } else {
+                // other error: quit immediately because something else is wrong
+                throw e
+            }
+        }
+    }
+}
+
+async function prefetchCoinGeckoPricesAndRWAMetadata(
+    cardanoClient: BlockfrostV0Client,
+    pricesToValidate: Record<string, PriceToValidate>
+): Promise<[Record<string, Record<string, number>>, Record<string, RWADatum>]> {
+    const coinGeckoIDs: Set<string> = new Set(["cardano"])
+    const rwas: Record<string, RWADatum> = {}
+
+    for (let name in pricesToValidate) {
+        if (name in COINGECKO_ASSETS) {
+            coinGeckoIDs.add(COINGECKO_ASSETS[name].coingeckoId)
+        } else {
+            const { assetClass } = pricesToValidate[name]
+            const metadata = await getRWAMetadata(cardanoClient, assetClass)
+
+            rwas[name] = metadata
+
+            // also add coinGeckoIDs for RWA reserves
+            coinGeckoIDs.add(getRWACoinGeckoID(metadata, assetClass))
+        }
+    }
+
+    // fetch all prices from CoinGecko at once
+    const coinGeckoResponse = await fetch(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${Array.from(coinGeckoIDs).join("%2C")}&vs_currencies=usd`
     )
+
+    const responseObj = await coinGeckoResponse.json()
+
+    return [responseObj, rwas]
+}
+
+function getRWACoinGeckoID(rwa: RWADatum, assetClass: AssetClass): string {
+    // how to verify the price?
+    switch (rwa.type) {
+        case "WrappedAsset":
+            if (!("venue" in rwa)) {
+                throw new Error(
+                    `venue not specified in metadata of ${assetClass.toString()}`
+                )
+            }
+
+            switch (rwa.venue) {
+                case "Bitcoin":
+                    switch (rwa.policy) {
+                        case "Native":
+                            return "bitcoin"
+                        default:
+                            throw new Error(
+                                `unhandled policy '${rwa.policy}' for Bitcoin RWA ${assetClass.toString()}`
+                            )
+                    }
+                case "Ethereum":
+                    switch (rwa.policy) {
+                        case "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": // USDC
+                            return "usd-coin"
+                        case "0x45804880De22913dAFE09f4980848ECE6EcbAf78": // PAXG
+                            return "pax-gold"
+                        default:
+                            throw new Error(
+                                `unhandled policy '${rwa.policy}' for RWA ${assetClass.toString()}`
+                            )
+                    }
+                default:
+                    throw new Error(
+                        `unhandled venue '${rwa.venue}' for RWA ${assetClass.toString()}`
+                    )
+            }
+        default:
+            throw new Error(
+                `only WrappedAsset RWA's supported, got ${rwa.type} for ${assetClass.toString()}`
+            )
+    }
+}
+
+// sync, because prices have been prefetched
+function validateCoinGeckoPrices(
+    coinGeckoPrices: Record<string, Record<string, number>>,
+    pricesToValidate: Record<string, PriceToValidate>,
+    validationErrors: Error[]
+): void {
+    for (let name in pricesToValidate) {
+        const { price } = pricesToValidate[name]
+
+        if (name in COINGECKO_ASSETS) {
+            validateCoinGeckoPrice(
+                coinGeckoPrices,
+                name,
+                price,
+                validationErrors
+            )
+
+            delete pricesToValidate[name]
+        }
+    }
 }
 
 async function validateRWAPrices(
-    cardanoClient: BlockfrostV0Client,
+    coinGeckoPrices: Record<string, Record<string, number>>,
+    rwas: Record<string, RWADatum>,
+    pricesToValidate: Record<string, PriceToValidate>,
+    validationErrors: Error[]
+): Promise<void> {
+    for (let name in pricesToValidate) {
+        const { price, assetClass } = pricesToValidate[name]
+
+        if (name in rwas) {
+            const rwa = rwas[name]
+
+            await validateRWAPrice(
+                coinGeckoPrices,
+                rwa,
+                assetClass,
+                price,
+                validationErrors
+            )
+
+            delete pricesToValidate[name]
+        }
+    }
+}
+
+// still async, because we must fetch actual reserves from other chains
+async function validateRWAPrice(
+    coinGeckoPrices: Record<string, Record<string, number>>,
+    metadata: RWADatum,
     assetClass: AssetClass,
     price: number,
     validationErrors: Error[]
 ) {
-    // it might be an internal bridged asset
-    const metadata = await getRWAMetadata(cardanoClient, assetClass)
-
     // how to verify the price?
     switch (metadata.type) {
         case "WrappedAsset":
@@ -339,6 +497,7 @@ async function validateRWAPrices(
             switch (metadata.venue) {
                 case "Bitcoin":
                     await validateBitcoinRWAPrices(
+                        coinGeckoPrices,
                         assetClass,
                         metadata,
                         price,
@@ -347,6 +506,7 @@ async function validateRWAPrices(
                     break
                 case "Ethereum":
                     await validateEthereumRWAPrices(
+                        coinGeckoPrices,
                         assetClass,
                         metadata,
                         price,
@@ -368,6 +528,7 @@ async function validateRWAPrices(
 }
 
 async function validateEthereumRWAPrices(
+    coinGeckoPrices: Record<string, Record<string, number>>,
     assetClass: AssetClass,
     metadata: RWADatumWrappedAsset,
     price: number,
@@ -384,7 +545,8 @@ async function validateEthereumRWAPrices(
 
     switch (metadata.policy) {
         case "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": // USDC
-            await validateWrappedUSDCPrice(
+            validateWrappedUSDCPrice(
+                coinGeckoPrices,
                 metadata,
                 reserves,
                 price,
@@ -392,7 +554,8 @@ async function validateEthereumRWAPrices(
             )
             break
         case "0x45804880De22913dAFE09f4980848ECE6EcbAf78": // PAXG
-            await validateWrappedPAXGPrice(
+            validateWrappedPAXGPrice(
+                coinGeckoPrices,
                 metadata,
                 reserves,
                 price,
@@ -407,6 +570,7 @@ async function validateEthereumRWAPrices(
 }
 
 async function validateBitcoinRWAPrices(
+    coinGeckoPrices: Record<string, Record<string, number>>,
     assetClass: AssetClass,
     metadata: RWADatumWrappedAsset,
     price: number,
@@ -421,6 +585,7 @@ async function validateBitcoinRWAPrices(
         case "Native":
             await validateWrappedBTCPrice(
                 provider,
+                coinGeckoPrices,
                 metadata,
                 price,
                 validationErrors
@@ -435,6 +600,7 @@ async function validateBitcoinRWAPrices(
 
 async function validateWrappedBTCPrice(
     provider: BitcoinWalletProvider,
+    coinGeckoPrices: Record<string, Record<string, number>>,
     metadata: RWADatumWrappedAsset,
     price: number,
     validationErrors: Error[]
@@ -442,6 +608,7 @@ async function validateWrappedBTCPrice(
     const reserves = BigInt(await provider.getSats())
 
     await validateWrappedTokenPriceWithCoingecko(
+        coinGeckoPrices,
         "bitcoin",
         metadata,
         reserves,
@@ -450,13 +617,15 @@ async function validateWrappedBTCPrice(
     )
 }
 
-async function validateWrappedUSDCPrice(
+function validateWrappedUSDCPrice(
+    coinGeckoPrices: Record<string, Record<string, number>>,
     metadata: RWADatumWrappedAsset,
     reserves: bigint,
     price: number,
     validationErrors: Error[]
 ) {
-    await validateWrappedTokenPriceWithCoingecko(
+    validateWrappedTokenPriceWithCoingecko(
+        coinGeckoPrices,
         "usd-coin",
         metadata,
         reserves,
@@ -465,13 +634,15 @@ async function validateWrappedUSDCPrice(
     )
 }
 
-async function validateWrappedPAXGPrice(
+function validateWrappedPAXGPrice(
+    coinGeckoPrices: Record<string, Record<string, number>>,
     metadata: RWADatumWrappedAsset,
     reserves: bigint,
     price: number,
     validationErrors: Error[]
 ) {
-    await validateWrappedTokenPriceWithCoingecko(
+    validateWrappedTokenPriceWithCoingecko(
+        coinGeckoPrices,
         "pax-gold",
         metadata,
         reserves,
@@ -480,19 +651,15 @@ async function validateWrappedPAXGPrice(
     )
 }
 
-async function validateCoingeckoPrice(
+function validateCoinGeckoPrice(
+    coinGeckoPrices: Record<string, Record<string, number>>,
     name: string,
     price: number,
     validationErrors: Error[]
 ) {
     const { coingeckoId } = COINGECKO_ASSETS[name]
 
-    // get the token price (TODO: all coingecko API calls at once)
-    const coinGeckoResponse = await fetch(
-        `https://api.coingecko.com/api/v3/simple/price?ids=cardano%2C${coingeckoId}&vs_currencies=usd`
-    )
-
-    const obj = await coinGeckoResponse.json()
+    const obj = coinGeckoPrices
 
     const usdPerAda = obj.cardano.usd
     const usdPerToken = obj[coingeckoId].usd
@@ -514,19 +681,15 @@ async function validateCoingeckoPrice(
  * @param price
  * @param validationErrors
  */
-async function validateWrappedTokenPriceWithCoingecko(
+function validateWrappedTokenPriceWithCoingecko(
+    coinGeckoPrices: Record<string, Record<string, number>>,
     coinGeckoID: string,
     metadata: RWADatumWrappedAsset,
     reserves: bigint,
     price: number,
     validationErrors: Error[]
 ) {
-    // get the token price (TODO: all coingecko API calls at once)
-    const coinGeckoResponse = await fetch(
-        `https://api.coingecko.com/api/v3/simple/price?ids=cardano%2C${coinGeckoID}&vs_currencies=usd`
-    )
-
-    const obj = await coinGeckoResponse.json()
+    const obj = coinGeckoPrices
 
     const usdPerAda = obj.cardano.usd
     const usdPerToken = obj[coinGeckoID].usd
